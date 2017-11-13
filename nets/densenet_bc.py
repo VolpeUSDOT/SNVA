d249d3044a248e63122e6c9a7570db07e6af2cb1
Franklin Abodo <franklin.abodo@dot.gov>
2017-11-13 14:44:32 -0500

    Refactored dropout to match tf-slim implementation. Explicitly excluding dropout when the rate is 0% is no longer required in the current version. Also renamted input parameters to inputs.

--- nets/densenet_bc.py
+++ nets/densenet_bc.py
@@ -28,43 +28,43 @@ def densenet_arg_scope(weight_decay=0.0001,
                        batch_norm_decay=0.997,
                        batch_norm_epsilon=1e-5,
                        batch_norm_scale=True):
-    """Defines the default DenseNet arg scope.
+  """Defines the default DenseNet arg scope.
 
-    Args:
-      weight_decay: The weight decay to use for regularizing the model.
-      batch_norm_decay: The moving average decay when estimating layer activation
-        statistics in batch normalization.
-      batch_norm_epsilon: Small constant to prevent division by zero when
-        normalizing activations by their variance in batch normalization.
-      batch_norm_scale: If True, uses an explicit `gamma` multiplier to scale the
-        activations in the batch normalization layer.
+  Args:
+    weight_decay: The weight decay to use for regularizing the model.
+    batch_norm_decay: The moving average decay when estimating layer activation
+      statistics in batch normalization.
+    batch_norm_epsilon: Small constant to prevent division by zero when
+      normalizing activations by their variance in batch normalization.
+    batch_norm_scale: If True, uses an explicit `gamma` multiplier to scale the
+      activations in the batch normalization layer.
 
-    Returns:
-      An `arg_scope` to use for the densenet models.
-    """
-    batch_norm_params = {
-        'decay': batch_norm_decay,
-        'epsilon': batch_norm_epsilon,
-        'scale': batch_norm_scale,
-        'updates_collections': tf.GraphKeys.UPDATE_OPS,
-    }
+  Returns:
+    An `arg_scope` to use for the densenet models.
+  """
+  batch_norm_params = {
+    'decay': batch_norm_decay,
+    'epsilon': batch_norm_epsilon,
+    'scale': batch_norm_scale,
+    'updates_collections': tf.GraphKeys.UPDATE_OPS,
+  }
 
-    with slim.arg_scope(
-            [slim.conv2d],
-            weights_regularizer=slim.l2_regularizer(weight_decay),
-            weights_initializer=slim.variance_scaling_initializer(),
-            activation_fn=tf.nn.relu,
-    ):
-        with slim.arg_scope([slim.batch_norm], **batch_norm_params):
-            with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
-                return arg_sc
+  with slim.arg_scope(
+          [slim.conv2d],
+          weights_regularizer=slim.l2_regularizer(weight_decay),
+          weights_initializer=slim.variance_scaling_initializer(),
+          activation_fn=tf.nn.relu,
+  ):
+    with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+      with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
+        return arg_sc
 
 
 def densenet_bc(inputs,
                 num_classes=None,
                 is_training=True,
                 growth_rate=12,
-                drop_rate=0,
+                dropout_keep_prob=1.0,
                 depth=100,
                 for_imagenet=True,
                 reuse=None,
@@ -95,30 +95,27 @@ def densenet_bc(inputs,
     bottleneck = True
     N = int((depth - 4) / (6 if bottleneck else 3))
 
-    def single_layer(input, n_out_channels, drop_rate):
+    def single_layer(inputs, n_out_channels):
 
-        conv = slim.batch_norm(input, activation_fn=tf.nn.relu)
+        conv = slim.batch_norm(inputs, activation_fn=tf.nn.relu)
         conv = slim.conv2d(conv, n_out_channels, [3, 3], stride=[1, 1])
-        if drop_rate > 0:
-            conv = tf.nn.dropout(conv, drop_rate)
+        conv = slim.dropout(conv, dropout_keep_prob, is_training=is_training, scope='Dropout')
 
         out = tf.concat([tf.identity(input), conv], 3)
 
         return out
 
-    def bottleneck_layer(input, n_output_channels, drop_rate):
+    def bottleneck_layer(inputs, n_output_channels):
 
         inter_channels = 4 * n_output_channels
 
-        conv = slim.batch_norm(input, activation_fn=tf.nn.relu)
+        conv = slim.batch_norm(inputs, activation_fn=tf.nn.relu)
         conv = slim.conv2d(conv, inter_channels, [1, 1], stride=[1, 1])
-        if drop_rate > 0:
-            conv = tf.nn.dropout(conv, drop_rate)
+        conv = slim.dropout(conv, dropout_keep_prob, is_training=is_training, scope='Dropout')
 
         conv = slim.batch_norm(conv, activation_fn=tf.nn.relu)
         conv = slim.conv2d(conv, inter_channels, [3, 3], stride=[1, 1])
-        if drop_rate > 0:
-            conv = tf.nn.dropout(conv, drop_rate)
+        conv = slim.dropout(conv, dropout_keep_prob, is_training=is_training, scope='Dropout')
 
         out = tf.concat([tf.identity(input), conv], 3)
 
@@ -129,12 +126,11 @@ def densenet_bc(inputs,
     else:
         add = single_layer
 
-    def transition(input, n_output_channels, drop_rate):
+    def transition(inputs, n_output_channels):
 
-        conv = slim.batch_norm(input, activation_fn=tf.nn.relu)
+        conv = slim.batch_norm(inputs, activation_fn=tf.nn.relu)
         conv = slim.conv2d(conv, n_output_channels, [1, 1], stride=[1, 1])
-        if drop_rate > 0:
-            conv = tf.nn.dropout(conv, drop_rate)
+        conv = slim.dropout(conv, dropout_keep_prob, is_training=is_training, scope='Dropout')
         conv = slim.avg_pool2d(conv, [2, 2], stride=2)
 
         return conv
@@ -151,28 +147,28 @@ def densenet_bc(inputs,
                     net = slim.max_pool2d(net, [3, 3], stride=2)
 
                     for i in range(0, 6):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
-                    net = transition(net, math.floor(n_channels * reduction), drop_rate)
+                    net = transition(net, math.floor(n_channels * reduction))
                     n_channels = math.floor(n_channels * reduction)
 
                     for i in range(0, 12):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
-                    net = transition(net, math.floor(n_channels * reduction), drop_rate)
+                    net = transition(net, math.floor(n_channels * reduction))
                     n_channels = math.floor(n_channels * reduction)
 
                     for i in range(0, 36):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
-                    net = transition(net, math.floor(n_channels * reduction), drop_rate)
+                    net = transition(net, math.floor(n_channels * reduction))
                     n_channels = math.floor(n_channels * reduction)
 
                     for i in range(0, 24):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
                     net = slim.batch_norm(net, activation_fn=tf.nn.relu)
@@ -182,26 +178,25 @@ def densenet_bc(inputs,
                     net = slim.conv2d(net, n_channels, [3, 3], stride=1)
 
                     for i in range(0, N):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
-                    net = transition(net, math.floor(n_channels * reduction), drop_rate)
+                    net = transition(net, math.floor(n_channels * reduction))
                     n_channels = math.floor(n_channels * reduction)
 
                     for i in range(0, N):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
-                    net = transition(net, math.floor(n_channels * reduction), drop_rate)
+                    net = transition(net, math.floor(n_channels * reduction))
                     n_channels = math.floor(n_channels * reduction)
 
                     for i in range(0, N):
-                        net = add(net, growth_rate, drop_rate)
+                        net = add(net, growth_rate)
                         n_channels += growth_rate
 
                     net = slim.batch_norm(net, activation_fn=tf.nn.relu)
                     net = slim.avg_pool2d(net, [8, 8], stride=8)
-
 
                 if num_classes is not None:
                     net = slim.conv2d(net, num_classes, [1, 1], activation_fn=None,
@@ -209,8 +204,10 @@ def densenet_bc(inputs,
                 net = tf.squeeze(net, [1, 2], name='SpatialSqueeze')
                 # Convert end_points_collection into a dictionary of end_points.
                 end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+
                 if num_classes is not None:
                     end_points['Predictions'] = slim.softmax(net, scope='Predictions')
+
                 return net, end_points
 
 densenet_bc.default_image_size = 224
