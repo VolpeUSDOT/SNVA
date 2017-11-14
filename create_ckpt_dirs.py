592ebed8c0ff0976d6258290d94453a0ffd7e9e8
Franklin Abodo <franklin.abodo@dot.gov>
2017-11-13 19:43:01 -0500

    Added script to generate checkpoint folder structure for a new dataset

---
+++ create_ckpt_dirs.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--checkpoint_root_dir', '-crd',
+                    default='/media/data_1/snva/Checkpoints/fhwa')
+parser.add_argument('--dataset_name', '-dn', required=True)
+
+args = parser.parse_args()
+
+ckpt_dir = os.path.join(args.checkpoint_root_dir, args.dataset_name)
+
+if not os.path.exists(ckpt_dir):
+  os.mkdir(ckpt_dir)
+
+archs = ['densenet_bc_12_100', 'densenet_bc_24_50', 'inception_resnet_v2',
+         'inception_v3', 'inception_v4', 'mobilenet_v1']
+
+inits = ['random_init', 'transfer_init']
+
+vals = ['eval', 'test']
+
+for arch in archs:
+  arch_dir = os.path.join(ckpt_dir, arch)
+
+  if not os.path.exists(arch_dir):
+    os.mkdir(arch_dir)
+
+  for init in inits:
+    init_dir = os.path.join(arch_dir, init)
+
+    if not os.path.exists(init_dir):
+      os.mkdir(init_dir)
+
+    for val in vals:
+      val_dir = os.path.join(init_dir, val)
+
+      if not os.path.exists(val_dir):
+        os.mkdir(val_dir)
