e2b91f273999f831b8ce099c95dd92b414a7d97a
Franklin Abodo <franklin.abodo@dot.gov>
2018-02-11 08:25:07 -0500

    conv layer depths are now variable, similar to the inceptionv3 implementation.

--- nets/inception_resnet_v2.py
+++ nets/inception_resnet_v2.py
@@ -29,18 +29,30 @@ import tensorflow as tf
 slim = tf.contrib.slim
 
 
-def block35(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
+def block35(net,
+            scale=1.0,
+            activation_fn=tf.nn.relu,
+            scope=None,
+            reuse=None,
+            min_depth=8,
+            depth_multiplier=1.0):
     """Builds the 35x35 resnet block."""
+    if depth_multiplier <= 0:
+        raise ValueError('depth_multiplier is not greater than zero.')
+
+    def depth(d):
+        return max(int(d * depth_multiplier), min_depth)
+
     with tf.variable_scope(scope, 'Block35', [net], reuse=reuse):
         with tf.variable_scope('Branch_0'):
-            tower_conv = slim.conv2d(net, 32, 1, scope='Conv2d_1x1')
+            tower_conv = slim.conv2d(net, depth(32), 1, scope='Conv2d_1x1')
         with tf.variable_scope('Branch_1'):
-            tower_conv1_0 = slim.conv2d(net, 32, 1, scope='Conv2d_0a_1x1')
-            tower_conv1_1 = slim.conv2d(tower_conv1_0, 32, 3, scope='Conv2d_0b_3x3')
+            tower_conv1_0 = slim.conv2d(net, depth(32), 1, scope='Conv2d_0a_1x1')
+            tower_conv1_1 = slim.conv2d(tower_conv1_0, depth(32), 3, scope='Conv2d_0b_3x3')
         with tf.variable_scope('Branch_2'):
-            tower_conv2_0 = slim.conv2d(net, 32, 1, scope='Conv2d_0a_1x1')
-            tower_conv2_1 = slim.conv2d(tower_conv2_0, 48, 3, scope='Conv2d_0b_3x3')
-            tower_conv2_2 = slim.conv2d(tower_conv2_1, 64, 3, scope='Conv2d_0c_3x3')
+            tower_conv2_0 = slim.conv2d(net, depth(32), 1, scope='Conv2d_0a_1x1')
+            tower_conv2_1 = slim.conv2d(tower_conv2_0, depth(48), 3, scope='Conv2d_0b_3x3')
+            tower_conv2_2 = slim.conv2d(tower_conv2_1, depth(64), 3, scope='Conv2d_0c_3x3')
         mixed = tf.concat(axis=3, values=[tower_conv, tower_conv1_1, tower_conv2_2])
         up = slim.conv2d(mixed, net.get_shape()[3], 1, normalizer_fn=None,
                          activation_fn=None, scope='Conv2d_1x1')
@@ -50,16 +62,28 @@ def block35(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
     return net
 
 
-def block17(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
+def block17(net,
+            scale=1.0,
+            activation_fn=tf.nn.relu,
+            scope=None,
+            reuse=None,
+            min_depth=8,
+            depth_multiplier=1.0):
     """Builds the 17x17 resnet block."""
+    if depth_multiplier <= 0:
+        raise ValueError('depth_multiplier is not greater than zero.')
+    
+    def depth(d):
+        return max(int(d * depth_multiplier), min_depth)
+    
     with tf.variable_scope(scope, 'Block17', [net], reuse=reuse):
         with tf.variable_scope('Branch_0'):
-            tower_conv = slim.conv2d(net, 192, 1, scope='Conv2d_1x1')
+            tower_conv = slim.conv2d(net, depth(192), 1, scope='Conv2d_1x1')
         with tf.variable_scope('Branch_1'):
-            tower_conv1_0 = slim.conv2d(net, 128, 1, scope='Conv2d_0a_1x1')
-            tower_conv1_1 = slim.conv2d(tower_conv1_0, 160, [1, 7],
+            tower_conv1_0 = slim.conv2d(net, depth(128), 1, scope='Conv2d_0a_1x1')
+            tower_conv1_1 = slim.conv2d(tower_conv1_0, depth(160), [1, 7],
                                         scope='Conv2d_0b_1x7')
-            tower_conv1_2 = slim.conv2d(tower_conv1_1, 192, [7, 1],
+            tower_conv1_2 = slim.conv2d(tower_conv1_1, depth(192), [7, 1],
                                         scope='Conv2d_0c_7x1')
         mixed = tf.concat(axis=3, values=[tower_conv, tower_conv1_2])
         up = slim.conv2d(mixed, net.get_shape()[3], 1, normalizer_fn=None,
@@ -70,16 +94,28 @@ def block17(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
     return net
 
 
-def block8(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
+def block8(net,
+           scale=1.0,
+           activation_fn=tf.nn.relu,
+           scope=None,
+           reuse=None,
+           min_depth=8,
+           depth_multiplier=1.0):
     """Builds the 8x8 resnet block."""
+    if depth_multiplier <= 0:
+        raise ValueError('depth_multiplier is not greater than zero.')
+    
+    def depth(d):
+        return max(int(d * depth_multiplier), min_depth)
+
     with tf.variable_scope(scope, 'Block8', [net], reuse=reuse):
         with tf.variable_scope('Branch_0'):
-            tower_conv = slim.conv2d(net, 192, 1, scope='Conv2d_1x1')
+            tower_conv = slim.conv2d(net, depth(192), 1, scope='Conv2d_1x1')
         with tf.variable_scope('Branch_1'):
-            tower_conv1_0 = slim.conv2d(net, 192, 1, scope='Conv2d_0a_1x1')
-            tower_conv1_1 = slim.conv2d(tower_conv1_0, 224, [1, 3],
+            tower_conv1_0 = slim.conv2d(net, depth(192), 1, scope='Conv2d_0a_1x1')
+            tower_conv1_1 = slim.conv2d(tower_conv1_0, depth(224), [1, 3],
                                         scope='Conv2d_0b_1x3')
-            tower_conv1_2 = slim.conv2d(tower_conv1_1, 256, [3, 1],
+            tower_conv1_2 = slim.conv2d(tower_conv1_1, depth(256), [3, 1],
                                         scope='Conv2d_0c_3x1')
         mixed = tf.concat(axis=3, values=[tower_conv, tower_conv1_2])
         up = slim.conv2d(mixed, net.get_shape()[3], 1, normalizer_fn=None,
@@ -94,7 +130,9 @@ def inception_resnet_v2_base(inputs,
                              final_endpoint='Conv2d_7b_1x1',
                              output_stride=16,
                              align_feature_maps=False,
-                             scope=None):
+                             scope=None,
+                             min_depth=8,
+                             depth_multiplier=1.0):
     """Inception model from  http://arxiv.org/abs/1602.07261.
 
     Constructs an Inception Resnet v2 network from inputs to the given final
@@ -134,31 +172,37 @@ def inception_resnet_v2_base(inputs,
         end_points[name] = net
         return name == final_endpoint
 
+    if depth_multiplier <= 0:
+        raise ValueError('depth_multiplier is not greater than zero.')
+    
+    def depth(d):
+        return max(int(d * depth_multiplier), min_depth)
+    
     with tf.variable_scope(scope, 'InceptionResnetV2', [inputs]):
         with slim.arg_scope([slim.conv2d, slim.max_pool2d, slim.avg_pool2d],
                             stride=1, padding='SAME'):
             # 149 x 149 x 32
-            net = slim.conv2d(inputs, 32, 3, stride=2, padding=padding,
+            net = slim.conv2d(inputs, depth(32), 3, stride=2, padding=padding,
                               scope='Conv2d_1a_3x3')
             if add_and_check_final('Conv2d_1a_3x3', net): return net, end_points
 
             # 147 x 147 x 32
-            net = slim.conv2d(net, 32, 3, padding=padding,
+            net = slim.conv2d(net, depth(32), 3, padding=padding,
                               scope='Conv2d_2a_3x3')
             if add_and_check_final('Conv2d_2a_3x3', net): return net, end_points
             # 147 x 147 x 64
-            net = slim.conv2d(net, 64, 3, scope='Conv2d_2b_3x3')
+            net = slim.conv2d(net, depth(64), 3, scope='Conv2d_2b_3x3')
             if add_and_check_final('Conv2d_2b_3x3', net): return net, end_points
             # 73 x 73 x 64
             net = slim.max_pool2d(net, 3, stride=2, padding=padding,
                                   scope='MaxPool_3a_3x3')
             if add_and_check_final('MaxPool_3a_3x3', net): return net, end_points
             # 73 x 73 x 80
-            net = slim.conv2d(net, 80, 1, padding=padding,
+            net = slim.conv2d(net, depth(80), 1, padding=padding,
                               scope='Conv2d_3b_1x1')
             if add_and_check_final('Conv2d_3b_1x1', net): return net, end_points
             # 71 x 71 x 192
-            net = slim.conv2d(net, 192, 3, padding=padding,
+            net = slim.conv2d(net, depth(192), 3, padding=padding,
                               scope='Conv2d_4a_3x3')
             if add_and_check_final('Conv2d_4a_3x3', net): return net, end_points
             # 35 x 35 x 192
@@ -169,21 +213,21 @@ def inception_resnet_v2_base(inputs,
             # 35 x 35 x 320
             with tf.variable_scope('Mixed_5b'):
                 with tf.variable_scope('Branch_0'):
-                    tower_conv = slim.conv2d(net, 96, 1, scope='Conv2d_1x1')
+                    tower_conv = slim.conv2d(net, depth(96), 1, scope='Conv2d_1x1')
                 with tf.variable_scope('Branch_1'):
-                    tower_conv1_0 = slim.conv2d(net, 48, 1, scope='Conv2d_0a_1x1')
-                    tower_conv1_1 = slim.conv2d(tower_conv1_0, 64, 5,
+                    tower_conv1_0 = slim.conv2d(net, depth(48), 1, scope='Conv2d_0a_1x1')
+                    tower_conv1_1 = slim.conv2d(tower_conv1_0, depth(64), 5,
                                                 scope='Conv2d_0b_5x5')
                 with tf.variable_scope('Branch_2'):
-                    tower_conv2_0 = slim.conv2d(net, 64, 1, scope='Conv2d_0a_1x1')
-                    tower_conv2_1 = slim.conv2d(tower_conv2_0, 96, 3,
+                    tower_conv2_0 = slim.conv2d(net, depth(64), 1, scope='Conv2d_0a_1x1')
+                    tower_conv2_1 = slim.conv2d(tower_conv2_0, depth(96), 3,
                                                 scope='Conv2d_0b_3x3')
-                    tower_conv2_2 = slim.conv2d(tower_conv2_1, 96, 3,
+                    tower_conv2_2 = slim.conv2d(tower_conv2_1, depth(96), 3,
                                                 scope='Conv2d_0c_3x3')
                 with tf.variable_scope('Branch_3'):
                     tower_pool = slim.avg_pool2d(net, 3, stride=1, padding='SAME',
                                                  scope='AvgPool_0a_3x3')
-                    tower_pool_1 = slim.conv2d(tower_pool, 64, 1,
+                    tower_pool_1 = slim.conv2d(tower_pool, depth(64), 1,
                                                scope='Conv2d_0b_1x1')
                 net = tf.concat(
                     [tower_conv, tower_conv1_1, tower_conv2_2, tower_pool_1], 3)
@@ -198,14 +242,14 @@ def inception_resnet_v2_base(inputs,
 
             with tf.variable_scope('Mixed_6a'):
                 with tf.variable_scope('Branch_0'):
-                    tower_conv = slim.conv2d(net, 384, 3, stride=1 if use_atrous else 2,
+                    tower_conv = slim.conv2d(net, depth(384), 3, stride=1 if use_atrous else 2,
                                              padding=padding,
                                              scope='Conv2d_1a_3x3')
                 with tf.variable_scope('Branch_1'):
-                    tower_conv1_0 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
-                    tower_conv1_1 = slim.conv2d(tower_conv1_0, 256, 3,
+                    tower_conv1_0 = slim.conv2d(net, depth(256), 1, scope='Conv2d_0a_1x1')
+                    tower_conv1_1 = slim.conv2d(tower_conv1_0, depth(256), 3,
                                                 scope='Conv2d_0b_3x3')
-                    tower_conv1_2 = slim.conv2d(tower_conv1_1, 384, 3,
+                    tower_conv1_2 = slim.conv2d(tower_conv1_1, depth(384), 3,
                                                 stride=1 if use_atrous else 2,
                                                 padding=padding,
                                                 scope='Conv2d_1a_3x3')
@@ -230,20 +274,20 @@ def inception_resnet_v2_base(inputs,
             # 8 x 8 x 2080
             with tf.variable_scope('Mixed_7a'):
                 with tf.variable_scope('Branch_0'):
-                    tower_conv = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
-                    tower_conv_1 = slim.conv2d(tower_conv, 384, 3, stride=2,
+                    tower_conv = slim.conv2d(net, depth(256), 1, scope='Conv2d_0a_1x1')
+                    tower_conv_1 = slim.conv2d(tower_conv, depth(384), 3, stride=2,
                                                padding=padding,
                                                scope='Conv2d_1a_3x3')
                 with tf.variable_scope('Branch_1'):
-                    tower_conv1 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
-                    tower_conv1_1 = slim.conv2d(tower_conv1, 288, 3, stride=2,
+                    tower_conv1 = slim.conv2d(net, depth(256), 1, scope='Conv2d_0a_1x1')
+                    tower_conv1_1 = slim.conv2d(tower_conv1, depth(288), 3, stride=2,
                                                 padding=padding,
                                                 scope='Conv2d_1a_3x3')
                 with tf.variable_scope('Branch_2'):
-                    tower_conv2 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
-                    tower_conv2_1 = slim.conv2d(tower_conv2, 288, 3,
+                    tower_conv2 = slim.conv2d(net, depth(256), 1, scope='Conv2d_0a_1x1')
+                    tower_conv2_1 = slim.conv2d(tower_conv2, depth(288), 3,
                                                 scope='Conv2d_0b_3x3')
-                    tower_conv2_2 = slim.conv2d(tower_conv2_1, 320, 3, stride=2,
+                    tower_conv2_2 = slim.conv2d(tower_conv2_1, depth(320), 3, stride=2,
                                                 padding=padding,
                                                 scope='Conv2d_1a_3x3')
                 with tf.variable_scope('Branch_3'):
@@ -260,17 +304,21 @@ def inception_resnet_v2_base(inputs,
             net = block8(net, activation_fn=None)
 
             # 8 x 8 x 1536
-            net = slim.conv2d(net, 1536, 1, scope='Conv2d_7b_1x1')
+            net = slim.conv2d(net, depth(1536), 1, scope='Conv2d_7b_1x1')
             if add_and_check_final('Conv2d_7b_1x1', net): return net, end_points
 
         raise ValueError('final_endpoint (%s) not recognized', final_endpoint)
 
 
-def inception_resnet_v2(inputs, num_classes=1001, is_training=True,
+def inception_resnet_v2(inputs,
+                        num_classes=1001,
+                        is_training=True,
                         dropout_keep_prob=0.8,
                         reuse=None,
                         scope='InceptionResnetV2',
-                        create_aux_logits=True):
+                        create_aux_logits=True,
+                        min_depth=8,
+                        depth_multiplier=1.0):
     """Creates the Inception Resnet V2 model.
 
     Args:
@@ -287,21 +335,28 @@ def inception_resnet_v2(inputs, num_classes=1001, is_training=True,
       logits: the logits outputs of the model.
       end_points: the set of end_points from the inception model.
     """
-    end_points = {}
+    if depth_multiplier <= 0:
+        raise ValueError('depth_multiplier is not greater than zero.')
+
+    def depth(d):
+        return max(int(d * depth_multiplier), min_depth)
 
     with tf.variable_scope(scope, 'InceptionResnetV2', [inputs, num_classes],
                            reuse=reuse) as scope:
         with slim.arg_scope([slim.batch_norm, slim.dropout],
                             is_training=is_training):
-            net, end_points = inception_resnet_v2_base(inputs, scope=scope)
+            net, end_points = inception_resnet_v2_base(inputs,
+                                                       scope=scope,
+                                                       min_depth=min_depth,
+                                                       depth_multiplier=depth_multiplier)
 
             if create_aux_logits:
                 with tf.variable_scope('AuxLogits'):
                     aux = end_points['PreAuxLogits']
                     aux = slim.avg_pool2d(aux, 5, stride=3, padding='VALID',
                                           scope='Conv2d_1a_3x3')
-                    aux = slim.conv2d(aux, 128, 1, scope='Conv2d_1b_1x1')
-                    aux = slim.conv2d(aux, 768, aux.get_shape()[1:3],
+                    aux = slim.conv2d(aux, depth(128), 1, scope='Conv2d_1b_1x1')
+                    aux = slim.conv2d(aux, depth(768), aux.get_shape()[1:3],
                                       padding='VALID', scope='Conv2d_2a_5x5')
                     aux = slim.flatten(aux)
                     aux = slim.fully_connected(aux, num_classes, activation_fn=None,
